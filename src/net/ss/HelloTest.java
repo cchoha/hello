ef5291c09b2fb95d1987d99efffdbdf0928424bd
cchoha <cchoha@sicc.co.kr>
2012-11-12 21:07:55 +0900

    add GetMessage()

---
+++ src/net/ss/HelloTest.java
@@ -0,0 +1,10 @@
+package net.ss;
+
+import junit.framework.TestCase;
+
+public class HelloTest extends TestCase {
+	public void testGetMessage() throws Exception {
+		HelloWorld hello = new HelloWorld();
+		assertEquals("Hello World", hello.getMessage());
+	}
+}

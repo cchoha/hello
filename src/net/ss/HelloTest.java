751d89d2e4744f77b4033b6d2d7b72a686c91026
cchoha <cchoha@sicc.co.kr>
2012-11-12 21:16:06 +0900

    add more tests

--- src/net/ss/HelloTest.java
+++ src/net/ss/HelloTest.java
@@ -7,4 +7,18 @@ public class HelloTest extends TestCase {
 		HelloWorld hello = new HelloWorld();
 		assertEquals("Hello World", hello.getMessage());
 	}
+	public void testGetMessageWithName() throws Exception {
+		HelloWorld hello = new HelloWorld();
+		assertEquals("Hello steve", hello.getMessage("steve"));
+	}
+	public void testGetMessageWithNameOther() throws Exception {
+		HelloWorld hello = new HelloWorld();
+		assertEquals("Hello jobs", hello.getMessage("jobs"));
+	}
+	public void testGetMessageWithNameNull() throws Exception {
+		HelloWorld hello = new HelloWorld();
+		assertEquals("Hello World", hello.getMessage(null));
+	}
 }
+
+
